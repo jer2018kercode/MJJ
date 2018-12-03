2cdf7bee97e8a06850389e890d4ccea7ced3b36d
borges jeremy <borgesjere@hotmail.com>
2018-12-03 14:08:53 +0100

    encore

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
-  $('#submit-button').click(function () {
-    // Code...
-  });
+ $('#submit-button').click(function () {
+ let message =document.getElementById('search-bar').value;
+ alert (message);
+// 
+
+//function delete(message){
+ // let sup=new supExp()
+//}
+//let supprimer=document.getElementById('search-bar').value;
+//let dansP={
+
+//}
+//delete('search-bar');
+//function supprimer(text){removeClass(text);
+//}
+ });
 });
